19c8775cf83b485fcfa4c6cbbaa8bac6a1d1d139
Egle Paku <egle.pakutinskaite@gmail.com>
2023-10-23 21:42:29 +0300

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Formik, Form, Field } from "formik";
 import "./App.css";
